d7edef902b636e0d2a6677c4e2f6842898d2c5f7
bzinsky <bzinsky@gmail.com>
2023-08-10 11:10:08 -0400

    updated fetch

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ console.log("Kyle O'Hara")
 console.log("Bryan Zinsky")
 console.log("Akunna Ottih")
 
+let image = document.querySelector("#pokeImage");
 let lat = document.querySelector("#floatingInput");
 let long = document.querySelector("#floatingPassword");
 let button = document.querySelector(".button1");
@@ -12,32 +13,37 @@ button.addEventListener("click", function () {
     fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat.value}&longitude=${long.value}&hourly=temperature_2m`)
         .then(function (response) {
 
+
             return response.json()
 
         })
         .then(function (weather) {
             console.log(weather)
-            fetchPokemonByWeather()
+            fetch(data.hourly.temperature_2m[0])
+                .then((response) => response.json())
+                .then((dataFromWeatherFetch) => {
+                    console.log(dataFromWeatherFetch)
+                    console.log(dataFromWeatherFetch.data.hourly.temperature_2m[0])
+                    // fetchPokemonByWeather()
+                })
         })
 })
-
-async function fetchPokemonByWeather() {
-    let searchCriteria = 'sunny';
-    try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`);
-        const data = await response.json();
-        console.log(data.results)
-        let pokeFetch = await fetch(data.results[0].url)
-        let pokeData = await pokeFetch.json()
-        console.log(pokeData.sprites.back_default)
-        return data.results;
-    } catch (error) {
-        console.error('Error fetching Pokémon data:', error);
-        return null;
-    }
-}
-
-let temp = 34
+// async function fetchPokemonByWeather() {
+//     let searchCriteria = 'sunny';
+//     try {
+//         const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`);
+//         const data = await response.json();
+//         console.log(data.results)
+//         let pokeFetch = await fetch(data.results[0].url)
+//         let pokeData = await pokeFetch.json()
+//         console.log(pokeData.sprites.back_default)
+//         return data.results;
+//     } catch (error) {
+//         console.error('Error fetching Pokémon data:', error);
+//         return null;
+//     }
+// }
+let temp = 28
 
 if (temp > 30) {
 
@@ -50,8 +56,8 @@ if (temp > 30) {
                 .then((response) => response.json())
                 .then((dataFromSecondFetch) => {
                     console.log(dataFromSecondFetch)
-                    console.log(dataFromSecondFetch.sprite.front_default)
-                    // set image src here
+                    console.log(dataFromSecondFetch.sprites.front_default)
+                    image.src = (dataFromSecondFetch.sprites.front_default)// set image src here
 
                 });
         });
@@ -65,11 +71,13 @@ if (temp > 30) {
             // https://pokeapi.co/api/v2/pokemon/1/
             console.log(data.pokemon[0].pokemon.name, data.pokemon[0].pokemon.url)
             fetch(data.pokemon[0].pokemon.url)
-                .then((reponse) =.response.json())
+                .then((response) => response.json())
                 .then((data) => {
+                    image.src = (data.sprites.front_default)
                     //https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/4.png
-                    console.log(data.sprites.front_default)
-                    //set image src here
+                    // console.log(data.sprites.front_default)
+                    console.log(data)
+                    //set image src her
                 });
         });
 
